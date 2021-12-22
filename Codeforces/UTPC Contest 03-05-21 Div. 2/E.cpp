f3ec363000a7b888936d0c6fe8fbcbc5111fd878
vidut_206_CNH <83084183+vidut206@users.noreply.github.com>
2021-12-22 12:06:28 +0700

    Update E.cpp

--- Codeforces/UTPC Contest 03-05-21 Div. 2/E.cpp
+++ Codeforces/UTPC Contest 03-05-21 Div. 2/E.cpp
@@ -1,25 +1,69 @@
-#include<bits/stdc++.h>
-#pragma GCC optimize("O3")
+/* 
+	Author : vidut_206_CNH
+	Problem: https://codeforces.com/problemset/gymProblem/102986/E
+*/
+#include <bits/stdc++.h>
 using namespace std;
+#define int long long
+#define fi first
+#define se second
+#define pb push_back
+#define gcd(a,b) (__gcd(a,b))
+#define lcm(a,b) (a/gcd(a,b)*b)
+#define sz(x) (int)(x.size())
+#define fast_cin() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define INF 2e18
+#define db(x) cerr << "[" << "Line " << __LINE__ << " -- " << (#x) << " : " << x << "] "
 
-typedef long long ll;
+typedef pair<int,int> pii;
 
-const ll nMax = 5e6 + 1;
+const int MOD = 1e9 + 7;
+const int MAXN1 = 1e5+5;
+const int MAXN2 = 5e6+5;
 
-vector<ll> pref(nMax, 0);
+int t;
+bool p[MAXN2];
+int g[MAXN2];
+int cnt[MAXN2];
+int f[MAXN2];
 
-
-
-
-
-int main() {
-	ios::sync_with_stdio(0);
-	cin.tie(0); cout.tie(0);
-	ll t;
-	cin >> t;
-	pref[2] = 1;
-	while (t) {
-		solve();
-		--t;
+void pre_calc() {
+	
+	for(int i=2;i<=(int)5e6;++i) g[i] = i;
+	
+	for(int i=2;i*i<=(int)5e6;++i) {
+		if(!p[i]) {
+			for(int j=i*i;j <= (int)5e6;j += i) {
+				p[j] = 1;
+				while(g[j]%i==0) {
+					g[j] /= i;
+					cnt[j]++;
+				}
+			}
+		}
 	}
+	for(int i=2;i<=(int)5e6;++i) {
+		cnt[i] += (g[i] != 1);
+		f[i] = f[i-1] + cnt[i]; 
+	}
+}
+
+
+
+signed main() {
+	fast_cin();
+	pre_calc();
+	cin >> t;
+	
+	while(t--) {
+		int r,l;
+		cin >> r >> l;
+		cout << f[r] - f[l] << "\n";
+	}
+	
+	#ifndef LOCAL_DEFINE
+    cerr << "\nTime elapsed: " << 1.0 * clock() / CLOCKS_PER_SEC << " s.\n ";
+    #endif
+	
+	return 0;
 }
