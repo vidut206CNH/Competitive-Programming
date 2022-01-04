1d0fc078bb8b1f13ce1b2365df1c849c351ee3aa
vidut206CNH <vidut2006scolt@gmail.com>
2022-01-04 23:58:31 +0700

    update 04-01-2022

--- VNOJ/dquery.cpp
+++ VNOJ/dquery.cpp
@@ -11,7 +11,6 @@ using namespace std;
 #define lcm(a,b) (a/gcd(a,b)*b)
 #define sz(x) (int)(x.size())
 #define fast_cin() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
-#define INF 2e18
 #define db(x) cerr << "[" << "Line " << __LINE__ << " -- " << (#x) << " : " << x << "] "
 
 typedef pair<int,int> pii;
@@ -19,6 +18,7 @@ typedef pair<int,int> pii;
 const int MOD = 1e9 + 7;
 const int MAXN1 = 1e5+5;
 const int MAXN2 = 1e6+5;
+const int inf = 1e18;
 
 
 
