ee8fa088b2b24cf5fc2f3d2f7226f9b130978620
vidut206CNH <vidut2006scolt@gmail.com>
2022-01-10 08:54:25 +0700

    update 09-01-2022

--- CSES/1143.cpp
+++ CSES/1143.cpp
@@ -16,15 +16,55 @@ using namespace std;
 typedef pair<int,int> pii;
 
 const int MOD = 1e9 + 7;
-const int MAXN1 = 1e5+5;
+const int MAXN1 = 2e5+5;
 const int MAXN2 = 1e6+5;
 const int inf = 1e18;
 
+int n,q;
+int a[MAXN1];
+int st[3*MAXN1];
+
+void build(int node, int l, int r) {
+	if(l == r) {
+		st[node] = a[l];
+		return;
+	}
+	int mid = (l+r)>>1;
+	build(node << 1, l, mid);
+	build(node << 1|1, mid + 1, r);
+	st[node] = max(st[node << 1], st[node << 1|1]);
+}
+
+
+void get(int node, int l, int r, int& val) {
+	if(l == r) {
+		st[node] -= val;
+		cout << l << " ";
+		return;
+	}
+	int mid = (l+r) >> 1;
+	if(val <= st[node << 1]) get(node << 1, l, mid, val);
+	else get(node << 1|1, mid + 1, r, val);
+	
+	st[node] = max(st[node << 1], st[node <<1|1]);
+}
 
 signed main() {
 	fast_cin();
 	
+	cin >> n >> q;
+	for(int i=1;i<=n;++i) {
+		cin >> a[i];
+	}
 	
+	build(1,1,n);
+	
+	for(int i=1;i<=q;++i) {
+		int val;
+		cin >> val;
+		if(st[1] < val) cout << "0 ";
+		else get(1,1,n,val);
+	}
 	
 	
 	#ifndef LOCAL_DEFINE
